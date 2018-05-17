502093a77a73c81c47c865eeac825573bdd792a9
TravisDowney <travisdowney2@my.whccd.edu>
2018-05-17 14:14:51 -0700

    Commit of second example of Android Programming

---
+++ app/src/main/java/com/example/dondo/example2/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.dondo.example2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.example.dondo.example2.utilities.ActivityHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ScrollView mScroll;
+    private TextView mLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//      Initialize the logging components
+        mScroll = (ScrollView) findViewById(R.id.scrollLog);
+        mLog = (TextView) findViewById(R.id.tvLog);
+        mLog.setText("");
+
+    }
+
+    public void onRunBtnClick(View v) {
+        displayMessage("Must run example 2!!");
+    }
+
+    public void onClearBtnClick(View v) {
+        mLog.setText("");
+        mScroll.scrollTo(0, mScroll.getBottom());
+    }
+
+    public void displayMessage(String message) {
+        // mLog.append(message + "\n");
+        ActivityHelper.log(this, mLog, message, true);
+        mScroll.scrollTo(0, mScroll.getBottom());
+    }
+}
